163e4591e823acaa7de4444664668891247da380
m_isogai <m_isogai@m-isogainoMacBook-Air.local>
2024-01-31 18:18:34 +0900

    jsonの作成及び呼び出し実装

--- app/components/ToDoList.tsx
+++ app/components/ToDoList.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { Todo } from "../types";
 
-const ToDoList = () => {
-  return <div>ToDoList</div>;
+type TodoListProps = {
+  todos: Todo[];
+};
+
+const ToDoList = ({ todos }: TodoListProps) => {
+  return (
+    <ul className="space-y-3">
+      {todos.map((todo) => (
+        <li
+          key={todo.id}
+          className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow"
+        >
+          <span>{todo.text}</span>
+          <div>
+            <button className="text-green-500 mr-3">Edit</button>
+            <button className="text-red-500">Delete</button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default ToDoList;
